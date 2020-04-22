cee0f5a391e1b6d1cf63a1833a006d6d29db65f1
Rahul Pandey <hello@rkpandey.com>
2020-04-22 01:35:49 -0700

    Add image view with glide

--- app/src/main/java/edu/stanford/rkpandey/week3/MainActivity.kt
+++ app/src/main/java/edu/stanford/rkpandey/week3/MainActivity.kt
@@ -2,11 +2,16 @@ package edu.stanford.rkpandey.week3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import kotlinx.android.synthetic.main.activity_main.*
 
+private const val STANFORD_IMAGE_URL = "https://pangea.stanford.edu/sites/default/files/Stanford%20Campus.jpg"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Glide.with(this).load(STANFORD_IMAGE_URL).transform(RoundedCorners(100)).into(imageView)
     }
 }
