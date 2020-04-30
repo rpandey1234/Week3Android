b79760a28aadcd56f0b54c6dc16f3ab54ec80cf8
Rahul Pandey <hello@rkpandey.com>
2020-04-29 17:59:00 -0700

    End of lecture 4

--- app/src/main/java/edu/stanford/rkpandey/week3/ContactAdapter.kt
+++ app/src/main/java/edu/stanford/rkpandey/week3/ContactAdapter.kt
@@ -1,27 +1,35 @@
 package edu.stanford.rkpandey.week3
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_contact.view.*
 
 public class ContactAdapter(val context: Context, val contacts: List<Contact>) :
     RecyclerView.Adapter<ContactAdapter.ViewHolder>() {
 
+    // Expensive operation
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_contact, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = contacts.size
+
+    // In-expensive operation
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val contact = contacts[position]
+        holder.bind(contact)
+    }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // Bind the data into the itemView
+        fun bind(contact: Contact) {
+            itemView.tvName.text = contact.name
+            itemView.tvAge.text = "Age: ${contact.age}"
+        }
 
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("not implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("not implemented")
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented")
     }
 }
--- app/src/main/java/edu/stanford/rkpandey/week3/MainActivity.kt
+++ app/src/main/java/edu/stanford/rkpandey/week3/MainActivity.kt
@@ -1,28 +1,53 @@
 package edu.stanford.rkpandey.week3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Add RecyclerView AndroidX library to the Gradle build file - DONE
-        //Define a model class to use as the data source - DONE
+        // RecyclerView steps: https://guides.codepath.com/android/using-the-recyclerview#using-the-recyclerview
+        // 1. Add RecyclerView AndroidX library to the Gradle build file - DONE
+        // 2. Define a model class to use as the data source - DONE
         val contacts = createContacts()
-        //Add a RecyclerView to your activity to display the items - DONE
-        //Create a custom row layout XML file to visualize the item - DONE
-        //Create a RecyclerView.Adapter and ViewHolder to render the item
-        //Bind the adapter to the data source to populate the RecyclerView
+        // 3. Add a RecyclerView to your activity to display the items - DONE
+        // 4. Create a custom row layout XML file to visualize the item - DONE
+        // 5. Create a RecyclerView.Adapter and ViewHolder to render the item
+        val adapter = ContactAdapter(this, contacts)
+        // 6. Bind the adapter to the data source to populate the RecyclerView
+        rvContacts.adapter = adapter
+        rvContacts.layoutManager = LinearLayoutManager(this)
     }
 
     private fun createContacts(): List<Contact> {
         val contacts = mutableListOf<Contact>()
         for (i in 1..150) contacts.add(Contact("Person $i", i))
         return contacts
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.item_add) {
+            // Navigate to the new activity
+            Log.i(TAG, "Tapped on add menu option")
+            val addContactIntent = Intent(this, AddContactActivity::class.java)
+            startActivity(addContactIntent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
